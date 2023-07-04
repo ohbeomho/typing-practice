f54b15fc638933a02939c05db2ccded63ee9ff42
OhBeomho <tyler0322@naver.com>
2023-07-04 21:34:05 +0900

    init

---
+++ src/components/Layout.svelte
@@ -0,0 +1,63 @@
+<div class="wrapper">
+  <header>
+    <nav>
+      <a href="/" class="brand">Typing Practice</a>
+    </nav>
+  </header>
+  <main>
+    <slot />
+  </main>
+  <footer>
+    <p>Made by <a href="https://github.com/OhBeomho">OhBeomho</a></p>
+    <p>
+      Source on <a href="https://github.com/OhBeomho/typing-practice">GitHub</a>
+    </p>
+  </footer>
+</div>
+
+<style>
+  :global(body) {
+    margin: 0;
+    height: 100vh;
+  }
+
+  :global(a) {
+    text-decoration: none;
+    color: rgb(0, 100, 255);
+  }
+
+  :global(a:hover) {
+    text-decoration: underline;
+  }
+
+  .wrapper {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+
+  nav {
+    padding: 10px;
+    box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.15);
+    background-color: black;
+    color: white;
+  }
+
+  nav .brand {
+    color: white;
+    font-size: 25px;
+  }
+
+  main {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  footer {
+    background-color: gray;
+    text-align: center;
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,152 @@
+<script>
+  import { onMount } from "svelte"
+  import Layout from "../components/Layout.svelte"
+
+  async function generateWords() {
+    const response = await fetch(
+      `https://random-word-api.vercel.app/api?words=${wordCount}`
+    )
+    words = await response.json()
+  }
+
+  function onKeydown(e) {
+    if (!started) {
+      started = new Date().getTime()
+    }
+
+    if ((e.key === " " || e.key === "Enter") && e.target.value) {
+      if (e.target.value === words[currentWord]) {
+        correct++
+      } else {
+        incorrect++
+
+        acc -= Math.round((1 / wordCount) * 100)
+      }
+
+      currentWord++
+      e.target.value = currentValue = ""
+      e.preventDefault()
+
+      if (currentWord === words.length) {
+        const elapsedTime = (new Date().getTime() - started) / 1000 / 60
+        started = null
+
+        alert(
+          `Result\n\n${Math.round(
+            wordCount / elapsedTime
+          )} wpm\nAccuracy: ${acc}%\n${correct} correct words\n${incorrect} incorrect words`
+        )
+
+        acc = 100
+        correct = 0
+        incorrect = 0
+        currentWord = 0
+
+        words = undefined
+        generateWords()
+      }
+    }
+  }
+
+  function onInput(e) {
+    currentValue = e.target.value
+
+    if (
+      e.target.value &&
+      e.target.value !== words[currentWord].slice(0, e.target.value.length)
+    ) {
+      isCorrect = false
+    } else {
+      isCorrect = true
+    }
+  }
+
+  let words
+  let currentWord = 0
+  let isCorrect = true
+  let correct = 0,
+    incorrect = 0,
+    acc = 100,
+    wordCount = 30
+  let currentValue = ""
+  let started
+
+  onMount(generateWords)
+</script>
+
+<Layout>
+  <h1>Typing Practice</h1>
+  {#if !words}
+    <p style="color: gray">Loading words...</p>
+  {:else}
+    <p>
+      <b>{currentWord}</b> / {wordCount} words
+    </p>
+    <p style={{ fontsize: 12 }}>
+      Correct: <span style="color: rgb(0, 150, 0)">{correct}</span><br />
+      Incorrect: <span style="color: rgb(200, 0, 0)">{incorrect}</span><br />
+      Accuracy: <b>{acc}</b>%
+    </p>
+    <div class="word">
+      {words[currentWord]}
+      <span class="next">
+        {words[currentWord + 1] || ""}
+      </span>
+      <input on:keydown={onKeydown} on:input={onInput} />
+      <p
+        style={`min-height: 30px; color: ${
+          isCorrect ? "black" : "rgb(200, 0, 0)"
+        }`}
+      >
+        {currentValue}
+      </p>
+    </div>
+    <div class="focus">Click here to focus</div>
+  {/if}
+</Layout>
+
+<style>
+  .word {
+    position: relative;
+    padding: 20px;
+    font-size: 22px;
+    filter: blur(2px);
+    opacity: 0.7;
+    text-align: center;
+    width: 100vw;
+    max-width: 300px;
+    border: 1.5px solid black;
+  }
+
+  .focus {
+    opacity: 1;
+    position: relative;
+    top: -82px;
+    z-index: -1;
+  }
+
+  .next {
+    position: absolute;
+    right: 10px;
+    color: gray;
+  }
+
+  .word:has(input:focus) {
+    filter: none;
+    opacity: 1;
+  }
+
+  .word:has(input:focus) ~ .focus {
+    opacity: 0;
+  }
+
+  .word > input {
+    all: unset;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    opacity: 0;
+  }
+</style>
